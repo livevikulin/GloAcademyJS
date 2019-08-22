531f65a4c66d60ad16d0c897d0fd54ae6e5fadce
Vikulin Vitaly <livevikulin@gmail.com>
2019-08-22 23:24:53 +0700

    Выполнил первое задание к первому уроку

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let money = 10000;
+	income = 30000;
+	addExpenses = 5000;
+	deposit = 60000;
+	mission = 150000;
+	period = 60;
+
+alert('Привет, разрабам:)');
+console.log('Денег: ' +money+ '!');
+console.log('Доход: ' +income+ '!');
+console.log('Добавить: ' +addExpenses+ '!');
+console.log('Депозит: ' +deposit+ '!');
+console.log('Задача: ' +mission+ '!');
+console.log('Период: ' +period+ ' дней!');
