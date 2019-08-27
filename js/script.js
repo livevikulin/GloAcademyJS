9edcfed76a3e0223b077013c7ee127a6e3d50506
Vikulin Vitaly <livevikulin@gmail.com>
2019-08-27 23:58:46 +0700

    выполнил усложненное задание к 3 уроку

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//Урок 2
+//УРОК 2
 
 // let money = 50000;
 // 	income = 'home';
@@ -30,37 +30,51 @@
 // let str = result.toString();
 // console.log(str.substring(0, 2));
 
-//Урок 3
+//УРОК 3
 
-let money = +prompt('Каков ваш месячный доход?');
-	income = 'home';
-	addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую: ');
-	deposit = confirm('Есть ли у вас депозит в банке?');
+// let money = +prompt('Каков ваш месячный доход?');
+// 	income = 'home';
+// 	addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую: ');
+// 	deposit = confirm('Есть ли у вас депозит в банке?');
 
-	console.log(money);
-	console.log(income);
-	console.log(addExpenses.split());
+// 	console.log(money);
+// 	console.log(income);
+// 	console.log(addExpenses.split());
 
-let addExpenses1 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-	sale1 = +prompt('Во сколько это обойдется?');
-	addExpenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-	sale2 = +prompt('Во сколько это обойдется?');
-	budgetMonth = money - (sale1 + sale2);
-	mission = 500000 / budgetMonth;
-	budgetDay = Math.floor(budgetMonth / 30);
+// let addExpenses1 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+// 	sale1 = +prompt('Во сколько это обойдется?');
+// 	addExpenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+// 	sale2 = +prompt('Во сколько это обойдется?');
+// 	budgetMonth = money - (sale1 + sale2);
+// 	mission = 500000 / budgetMonth;
+// 	budgetDay = Math.floor(budgetMonth / 30);
 
-console.log(budgetMonth);
-console.log(Math.ceil(mission));
-console.log(budgetDay);
+// console.log(budgetMonth);
+// console.log(Math.ceil(mission));
+// console.log(budgetDay);
 
-if (budgetDay > 800) {
-	console.log('Высокий уровень дохода');
-} else if (budgetDay >= 300 && budgetDay <= 800) {
-	console.log('Средний уровень дохода');
-} else if (budgetDay >= 0 && budgetDay <= 300) {
-	console.log('Низкий уровень дохода');
-} else {
-	console.log('Что то пошло не так');
-}
+// if (budgetDay > 800) {
+// 	console.log('Высокий уровень дохода');
+// } else if (budgetDay >= 300 && budgetDay <= 800) {
+// 	console.log('Средний уровень дохода');
+// } else if (budgetDay >= 0 && budgetDay <= 300) {
+// 	console.log('Низкий уровень дохода');
+// } else {
+// 	console.log('Что то пошло не так');
+// }
 
+//УРОК 3(сложное задание)
+let lang = 'en';
+let arr = [];
+
+arr['ru'] = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+arr['en'] = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
+
+console.log(arr[lang]);
+
+let namePerson = prompt('Введите имя и узнаете кто этот человек.');
+let result = namePerson == 'Артем' ? 'Директор':
+			 namePerson == 'Максим' ? 'Преподаватель':
+			 'Введите имя Артем или Максим';
+console.log(result);
 
